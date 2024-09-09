c52967066c10ba4f299ed3e058edf690900ef01a
IrickMarvinGalan <qimagalan@tip.edu.ph>
2024-09-09 12:34:06 +0800

    Add files via upload

---
+++ Translated Algorithm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	// Initializing Variables
+	int x = 4, y=2;
+	
+	// Initializing Formulas
+	int sum = x + y; 
+	int difference = x - y; 
+	int product = x * y; 
+	int quotient = x / y;
+	int remainder = x % y;
+	
+	// Output of Procedure
+	cout << "Sum: " << sum << endl;
+	cout << "Difference: " << difference << endl;
+	cout << "Product: " << product << endl;
+	cout << "Quotient: " << quotient << endl;
+	cout << "Remainder: " << remainder << endl;
+	
+	return 0;
+}

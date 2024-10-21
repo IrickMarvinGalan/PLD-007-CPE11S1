3957ce0c9fdc2ca67d7b882f7375398823bd2162
IrickMarvinGalan <qimagalan@tip.edu.ph>
2024-10-21 11:11:06 +0800

    Update Exercise 6.cpp

--- Exercise 6.cpp
+++ Exercise 6.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 
 
-int linearSearch(int arr[], int size, int target) {
-	
-	for (int i = 0; i < size; i++) {
-		
-		if (arr[i] == target) {
+int linearSearch(int arr[], int size, int target) 
+{
+	for (int i = 0; i < size; i++) 
+	{	
+		if (arr[i] == target) 
+		{
 			return i;
 		}
-		
 	}
 	
-	return -1;
-	
+	return -1;	
 }
 
-int main () {
+int main () 
+{
 	
 	int arr[] = {5, 3, 8, 4, 2};
 	int size = sizeof(arr) / sizeof(arr[0]);
@@ -29,12 +29,13 @@ int main () {
 	
 	int result = linearSearch(arr, size, target);
 	
-	if (result != -1) {
-		
+	if (result != -1) 
+	{
 		cout << "Index that the Value is Found: " << result;
-		
-	} else {
+	} else 
+	{
 		cout << "Value not Found!"; 
 	}
-	
+
+	return 0;
 }

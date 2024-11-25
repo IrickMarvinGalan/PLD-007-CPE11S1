6d4fded0e74c6a7f32d7dbf5e437c2feea611acb
IrickMarvinGalan <qimagalan@tip.edu.ph>
2024-11-25 12:26:59 +0800

    Create Exercise 8.cpp

---
+++ FINALS/Exercise 8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+int main () {
+	//Exercise 8
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+	cout << "You entered: " << num << endl;
+	
+	return 0;
+}
